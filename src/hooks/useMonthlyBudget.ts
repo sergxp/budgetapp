5687ab0872a6b0a814f36255dbe27509a13ff1ee
Serge Petrenko <sergxpxp@gmail.com>
2022-08-18 23:07:20 -0500

    Lots of shit some more

--- src/hooks/useMonthlyBudget.ts
+++ src/hooks/useMonthlyBudget.ts
@@ -1,14 +1,28 @@
-import { BudgetDay } from "@prisma/client";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
+import { BudgetDayDto } from "../../application/models/budgetDayDto";
+import { BudgetDay } from "../../domain/BudgetDay";
+import { MonthlyBudget } from "../../domain/MonthlyBudget";
 
 export const useMonthlyBudget = (month: number) => {
-  return useQuery(["budget"], () => {
+  const { isLoading, data } = useQuery(["budget"], () => {
     return axios
-      .get<BudgetDay[]>(
+      .get<BudgetDayDto[]>(
         `${process.env.NEXT_PUBLIC_API_BASE}/budget/month/${month}`
       )
-      .then((res) => res.data);
+      .then((res) => res.data.map((day) => BudgetDay.fromDto(day)));
   });
+
+  const [monthlyBudget, setMonthlyBudget] = useState<MonthlyBudget>();
+
+  useEffect(() => {
+    if (data) {
+      setMonthlyBudget(new MonthlyBudget(data, 0));
+    }
+  }, [data]);
+
+  return {
+    monthlyBudget,
+  };
 };
